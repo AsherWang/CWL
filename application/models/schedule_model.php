3a17e1cf7aff7975af05eda4f398b9050d5f66ef
AsherWang <1057488870@qq.com>
2014-12-01 02:39:21 +0800

    添加

---
+++ application/models/schedule_model.php
@@ -0,0 +1,15 @@
+<?php 
+	class Schedule_model extends CI_Model{
+
+		var $ID='';
+		var $DateList='';
+		var $Time='';
+		var $Special='';
+		
+		function _construct()
+		{
+			parent::_construct();
+		}
+
+	}
+?>
---
+++ application/models/user_ext_model.php
@@ -0,0 +1,15 @@
+<?php 
+	class User_ext_model extends CI_Model{
+
+		var $ID='';
+		var $Phone='';
+		var $Adress='';
+		var $Sex='';
+		
+		function _construct()
+		{
+			parent::_construct();
+		}
+	}
+	
+?>
