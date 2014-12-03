85547b189b7159746bbddef5584987ed56a30f7c
AsherWang <1057488870@qq.com>
2014-12-03 17:14:06 +0800

    'Asher-update'

--- application/controllers/TestControl.php
+++ application/controllers/TestControl.php
@@ -3,7 +3,7 @@
  * @Author: Asher
  * @Date:   2014-12-03 15:31:08
  * @Last Modified by:   Asher
- * @Last Modified time: 2014-12-03 17:05:19
+ * @Last Modified time: 2014-12-03 17:13:57
  */
 class TestControl extends CI_Controller {
   public function __construct()
@@ -19,7 +19,7 @@ class TestControl extends CI_Controller {
     $data['title'] = 'testpage';
     $data["value"]=$this->user_model->user_login("小李","lovelive");
     $this->load->view('TestPage/Index', $data);
-
+      
   }
 }
 ?>
