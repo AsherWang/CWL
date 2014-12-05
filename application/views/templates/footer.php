efb6b514d7f1dad30527d32baf3d37d15ff4e09f
2205 <13141454996@163.com>
2014-12-05 16:24:21 +0800

    以惭矫

--- application/views/templates/footer.php
+++ application/views/templates/footer.php
@@ -1,3 +1,5 @@
-<strong>&copy,2014</strong>
+<div  style="width:100%;background-color:black;text-align:center;">
+<span style="font-weight:400;font-size:25px;">&copy,2014<br/>早晚得改</span>
+</div>
 </body>
 </html>
