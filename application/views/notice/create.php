2dab153d85f64e5938e7b9b078bbc09555c2002f
TaoPeng <1037137377@qq.com>
2014-12-15 16:29:12 +0800

    tp
    Signed-off-by: TaoPeng <1037137377@qq.com>

--- application/views/notice/create.php
+++ application/views/notice/create.php
@@ -1,20 +1,22 @@
 <!DOCTYPE html>
 <html>
-<head>
+<head lang="en">
 	<meta charset="utf-8">
+	<link href='<?php echo base_url().'/res/css/create.css'?>' rel="stylesheet" type="text/css" />
 	<title>发布公告</title>
 </head>
 <body>
-<h2>发布一条公告</h2>
-<?php echo validation_errors();?>
-<?php echo form_open('notice/create')?>
-<label for="title">标题</label>
-<input type="input" name="title" /><br/>
-
-<label for="content">公告内容</label>
-<textarea name="content"></textarea><br/>
-
-<input type="submit" name="submit" value="发布公告"/>
-</form>
+    <h2 align="center">发布一条公告</h2>
+    <?php echo validation_errors();?>
+    <?php echo form_open('notice/create')?>
+    <form>
+        <div >
+        <label for="title">标题</label>
+        <input type="input" name="title" /><br/>
+        <label for="content">公告内容</label>
+        <textarea name="content"></textarea><br/>
+        <input type="submit" name="submit" value="发布公告"/>
+        </div>
+    </form>
 </body>
 </html>
