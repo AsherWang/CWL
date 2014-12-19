e8ec1ac71caae836842e80b8ac79a6ed2da0f90d
kdestiny <1378968221@qq.com>
2014-12-19 22:49:21 +0800

    界面改动.
    tc

--- application/views/HospitalManangerPage/Index.php
+++ application/views/HospitalManangerPage/Index.php
@@ -14,18 +14,37 @@
      <link rel="stylesheet" href='<?php echo base_url().'res/css/bootstrap.min.css'?>' type="text/css" /> 
      <link rel="stylesheet" href='<?php echo base_url().'res/css/bootstrap.css'?>' type="text/css" /> 
 </head> 
-  <body> 
-  <div class="container">
-<p>欢迎您，管理员 <u><?php echo $admin_name;?></u></p>
- 
-            <div style="float: left">
-          <ul style="list-style:none; float:left;">
-            <li style="float:left;"><a class="btn btn-primary" style="width:200px" href="<?php echo base_url()?>HospitalManangerPage/">医院信息修改</a></li>
-              <li style="float:left;"><a class="btn btn-primary" style="width:200px" href="<?php echo base_url()?>HospitalManangerPage/UserManagee">挂号处人员管理</a></li>
-              <li style="float:left;"><a class="btn btn-primary" style="width:200px" href="<?php echo base_url()?>">医院公告管理</a></li>
-                            <li style="float:left;"><a class="btn btn-primary" style="width:200px" href="<?php echo base_url()?>HospitalManangerPage?do=exit">退出管理</a></li>
-          </ul>
+  <body>
+  <p>欢迎您，管理员 <u><?php echo $admin_name;?></u></p>
+  <div class="navbar navbar-default navbar-fixed-top" role="navigation">
+      <div class="container">
+          <div class="navbar-header">
+              <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+                  <span class="sr-only">Toggle navigation</span>
+                  <span class="icon-bar"></span>
+                  <span class="icon-bar"></span>
+                  <span class="icon-bar"></span>
+              </button>
+              <a class="navbar-brand" href="#">CWL全国统一预约平台</a>
+          </div>
+          <div class="navbar-collapse collapse">
+              <ul class="nav navbar-nav">
+                  <li class="active"><a href="#">医院信息修改</a></li>
+                  <li class="divider-vertical"></li>
+                  <li><a href="#about">挂号处人员管理</a></li>
+                  <li><a href="#contact">医院公告管理</a></li>
+                  <li><a href="#contact">退出管理</a></li>
+
+              </ul>
+
+          </div>
       </div>
-      
   </div>
+
+
+
+
+ 
+     
+
    <div>调试信息<?php var_dump($debug_value);?></div>
