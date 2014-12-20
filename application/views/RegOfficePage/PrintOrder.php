110c6fff8740e49dc9c4dc068825ef3430d51655
AsherWang <1057488870@qq.com>
2014-12-21 01:11:08 +0800

    'Asher-update'

--- application/views/RegOfficePage/PrintOrder.php
+++ application/views/RegOfficePage/PrintOrder.php
@@ -7,7 +7,7 @@
             eprnstr="<!--endprint"+oper+"-->";//设置打印结束区域
             prnhtml=bdhtml.substring(bdhtml.indexOf(sprnstr)+18); //从开始代码向后取html
             prnhtml=prnhtml.substring(0,prnhtml.indexOf(eprnstr));//从结束代码向前取html
-            window.document.body.innerHTML=prnhtml;
+            window.document.body.innerHTML="<style type='text/css'>.printContent{border:1px black solid;}  </style>"+prnhtml;
             window.print();
             window.document.body.innerHTML=bdhtml;
                 }
@@ -16,15 +16,13 @@
         
     <p>啊哈哈哈</p>
     <!--startprint1-->
-    <div style="boder:1px black solid;">
-    	<p>传说中的挂号单</p>
-    	<div>
-        	<p>一去二三里</p>
-            <p>烟村四五家</p>
-            <p>亭台六七座</p>
-            <p>都不是我的</p>
-            <p>哈哈哈</p>
-        </div>
+    <div >
+    	<table class="printContent" width=600 height=200>
+        	<tr><td colspan="2" align="center">挂号单<td></tr>
+            <tr><td align="center">挂号单ID<td><td>11</td></tr>
+            <tr><td colspan="2" align="center">挂号单<td></tr>
+            
+        </table>
  	</div>
     <!--endprint1-->
     <input type="button" onclick="preview(1)" value="哈哈" />
