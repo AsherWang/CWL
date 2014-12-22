51ad4cfece343f4f443a8a4f88f193f27a05fd11
TaoPeng <1037137377@qq.com>
2014-12-22 18:12:25 +0800

    tp

--- application/views/templates/noticeheader.php
+++ application/views/templates/noticeheader.php
@@ -43,35 +43,43 @@ if(isset($session->userdata['user_type']))
     </a>
     <div class="col-md-4 col-md-offset-5" id="HeaderRightDiv">
     <?php if(!$log){ ?>
-  		<a class="HeaderText" id="HeaderRegisterLink" style="cursor:pointer;position:absolute;left:-100px" data-toggle="modal" data-target="#myModal">发布公告</a>
   		<a class="HeaderText" id="HeaderLoginLink" href="<?php echo base_url()?>IndexPage/Index">登录</a>
         <a class="HeaderText" id="HeaderRegisterLink" href="<?php echo base_url()?>###">注册</a>
-        <div class="modal fade" id="myModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
-            <div class="modal-dialog">
-                <div class="modal-content">
-                    <div class="modal-header">
-                        <button type="button" class="close" data-dismiss="modal"><span aria-hidden="true">&times;</span><span class="sr-only">Close</span></button>
-                        <h4 class="modal-title">发布新的公告</h4>
-                    </div>
-                    <div class="modal-body">
-                        <p>公告标题：</p>
-                        <input type="text" class="form-control" id="CreateNoticeTitle">
-                        <p>公告内容: </p>
-                        <textarea id="CreateNoticeContentTextarea"></textarea>
-                    </div>
-                    <div class="modal-footer">
-                        <button type="button" class="btn btn-default" data-dismiss="modal">取消</button>
-                        <button type="button" class="btn btn-primary">确认发布</button>
-                    </div>
-                </div><!-- /.modal-content -->
-            </div><!-- /.modal-dialog -->
-        </div><!-- /.modal -->
   	<?php }else{ ?>
-      <a class="HeaderText" id="HeaderToNoticeLink" href="<?php echo base_url()?>NoticePage/index">查看公告</a>
-  		<span class="HeaderStaticText HeaderDivide">|</span>
-      <a class="HeaderText" id="HeaderMyOrderLink" href="<?php echo base_url()?>RegUserPage/My_appointment">我的预约单</a>
-      <span class="HeaderStaticText HeaderDivide">|</span>
-      <a class="HeaderText" id="HeaderNameLink" href="<?php echo base_url()?>RegUserPage/Personal_message_change"><?php echo $session->userdata['username'];?></a>
+        <?php if($session->userdata['user_type'] == "1" || $session->userdata['user_type'] == "2"){ ?>
+          <a class="HeaderText" id="HeaderRegisterLink" style="cursor:pointer" data-toggle="modal" data-target="#myModal">发布公告</a>
+          <div class="modal fade" id="myModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+                <div class="modal-dialog">
+                    <div class="modal-content">
+                        <div class="modal-header">
+                            <button type="button" class="close" data-dismiss="modal"><span aria-hidden="true">&times;</span><span class="sr-only">Close</span></button>
+                            <h4 class="modal-title">发布新的公告</h4>
+                        </div>
+                        <div class="modal-body">
+                            <p>公告标题：</p>
+                            <input type="text" class="form-control" id="CreateNoticeTitle">
+                            <p>公告内容: </p>
+                            <textarea id="CreateNoticeContentTextarea"></textarea>
+                        </div>
+                        <div class="modal-footer">
+                            <button type="button" class="btn btn-default" data-dismiss="modal">取消</button>
+                            <button type="button" class="btn btn-primary">确认发布</button>
+                        </div>
+                    </div><!-- /.modal-content -->
+                </div><!-- /.modal-dialog -->
+          </div><!-- /.modal -->
+            <span class="HeaderStaticText HeaderDivide">|</span>
+          <a class="HeaderText" id="HeaderToNoticeLink" href="<?php echo base_url()?>NoticePage/index">查看公告</a>
+            <span class="HeaderStaticText HeaderDivide">|</span>
+          <a class="HeaderText" id="HeaderNameLink" href="<?php echo base_url()?>RegUserPage/Personal_message_change"><?php echo $session->userdata['username'];?></a>
+        <?php }?>
+        <?php if($session->userdata['user_type'] == "3" || $session->userdata['user_type'] == "4"){ ?>
+              <a class="HeaderText" id="HeaderToNoticeLink" href="<?php echo base_url()?>NoticePage/index">查看公告</a>
+                <span class="HeaderStaticText HeaderDivide">|</span>
+              <a class="HeaderText" id="HeaderMyOrderLink" href="<?php echo base_url()?>RegUserPage/My_appointment">我的预约单</a>
+              <span class="HeaderStaticText HeaderDivide">|</span>
+              <a class="HeaderText" id="HeaderNameLink" href="<?php echo base_url()?>RegUserPage/Personal_message_change"><?php echo $session->userdata['username'];?></a>
+        <?php }?>
     <?php }?>
     <?php if($isSuper){ ?>
       <a class="HeaderText" id="HeaderSuperLink" href="<?php echo base_url()?>SuperManagerPage/Index">权限狗专区</a>
@@ -79,3 +87,4 @@ if(isset($session->userdata['user_type']))
     </div>
   	<hr class="HeaderFooterHr" id="HeaderHr"/>
   </div>
+  <?php var_dump($session->userdata["user_type"]);?>
